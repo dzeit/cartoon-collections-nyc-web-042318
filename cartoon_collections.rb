97177133fe4f1868afa4bf54eac060fc1dd9335a
David Zeit <dzeit216@gmail.com>
2018-04-18 22:51:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ end
 
 def long_planeteer_calls(word_calls)
   long_calls = word_calls.map { |call| call.length >= 4 }
+  binding.pry
   # ternary: word_calls >= 4 ? true : false
   if long_calls.any? { |calls| calls == true } #=> returns true or false
     true
