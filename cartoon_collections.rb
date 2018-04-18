32fd6fad7e270e13aa084aab09bef5b4ff283c9c
David Zeit <dzeit216@gmail.com>
2018-04-18 23:00:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,14 +10,16 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(word_calls)
-  long_calls = word_calls.map { |call| call.length > 4 }
-  #binding.pry
-  # ternary: word_calls > 4 ? true : false
-  if long_calls.any? { |calls| calls == true } #=> returns true or false
-    true
-  else
-    false
-  end
+  # long_calls = word_calls.map { |call| call.length > 4 }
+  # #binding.pry
+  # # ternary: word_calls > 4 ? true : false
+  # if long_calls.any? { |calls| calls.length == true } #=> returns true or false
+  #   true
+  # else
+  #   false
+  # end
+
+  word_calls.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
